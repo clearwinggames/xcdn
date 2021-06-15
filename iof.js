55712d9f7312a31d94d6673873c986047a4016f4
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2021-06-15 17:47:06 -0400

    Create iof.js

---
+++ iof.js
@@ -0,0 +1,6 @@
+HTMLDivElement.prototype.divAction = function() {
+    console.log('Div Action Here');
+};
+HTMLSpanElement.prototype.spanAction = function() {
+    console.log('Span Action Here');
+};
