8be5c6667bd13ffcfbd52933f2f71172d8eb6460
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2021-06-15 18:03:26 -0400

    Update iof.js

--- iof.js
+++ iof.js
@@ -2,9 +2,22 @@ Object.prototype.isInstanceOf = function( type ) {
     return this instanceof type;
 };
 
+Object.prototype.instanceType = function () {
+    let types = listDOMTypes();
+    for(let i = 0; i < types.length; i++) {
+        if (this instanceof types[i]) return types[i];   
+    }
+    return null;
+};
+
 HTMLDivElement.prototype.divAction = function() {
     console.log('Div Action Here');
 };
 HTMLSpanElement.prototype.spanAction = function() {
     console.log('Span Action Here');
+};
+function listDOMTypes() {
+    return [
+                HTMLDivElement, HTMLSpanElement
+        ];
 };
