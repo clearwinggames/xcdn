cea2f5a742166acea3d7f48d991e22dfc5e6ff7e
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2021-06-15 20:26:26 -0400

    Update scripio.js

--- scripio.js
+++ scripio.js
@@ -1,16 +1,25 @@
 function loadio(url, implementationCode, location){
 
-    let scriptTag = document.createElement('script');
-    scriptTag.src = url;
-    
-    if (typeof implementationCode != 'undefined') {
+    return new Promise(function(resolve, reject) 
+    {
         
-        scriptTag.onload = implementationCode;
-        scriptTag.onreadystatechange = implementationCode;
-    }
+        let scriptTag = document.createElement('script');
+        scriptTag.src = url;
+    
+        if (typeof implementationCode != 'undefined') {
+        
+            scriptTag.onload = implementationCode;
+            scriptTag.onreadystatechange = implementationCode;
+        }
+        else {
 
-    if (typeof location != 'undefined')
-        location.appendChild(scriptTag);
-    else
-        document.body.appendChild(scriptTag);
+        }
+
+        if (typeof location != 'undefined') 
+            location.appendChild(scriptTag);
+        else
+            document.body.appendChild(scriptTag);
+        
+        resolve(scriptTag);
+    });
 };
