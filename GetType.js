4a0d88928f5052f5a063f1948fddcab415f7a43c
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2021-07-17 12:52:45 -0400

    Update GetType.js

--- GetType.js
+++ GetType.js
@@ -3,3 +3,7 @@ function GetType(obj){
    var results = (funcNameRegex).exec((obj).constructor.toString());
    return (results && results.length > 1) ? results[1] : "";
 }
+function typeOf(obj){
+    var results = (/function (.{1,})\(/).exec((obj).constructor.toString());
+   return (results && results.length > 1) ? results[1] : "";
+}
