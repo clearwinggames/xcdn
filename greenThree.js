5ded86718b274556b54baa008ad1be7ca08dc52e
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2021-10-29 20:25:09 -0400

    Create greenThree.js

---
+++ greenThree.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My first three.js app</title>
+		<style>
+			body { margin: 0; }
+		</style>
+	</head>
+	<body>
+    
+    <script src="https://clearwinggames.github.io/xcdn/three.js">
+    </script>
+
+    <script src="https://clearwinggames.github.io/xcdn/baseThree.js">
+    </script>
+
+	</body>
+</html>
+© 2021 GitHub, Inc.
+Terms
+Privacy
