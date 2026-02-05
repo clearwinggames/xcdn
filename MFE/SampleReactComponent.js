0f0a2fafa6c064c1ab902aeb278b58cf189b9edc
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-05 10:52:22 -0500

    Create SampleReactComponent.js

---
+++ MFE/SampleReactComponent.js
@@ -0,0 +1,21 @@
+//import Greeting from './Greeting';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Parent Component</h1>
+      {/* Pass two parameters as props */}
+      <Greeting name="Alex" age={30} />
+      <Greeting name="Maria" age={25} />
+    </div>
+  );
+}
+// Destructure the 'props' object directly in the function parameters
+function Greeting({ name, age }) {
+  return (
+    <p>
+      Hello, {name}! You are {age} years old.
+    </p>
+  );
+}
+export default Greeting;
