33433d363da581a107ac1eea7997c7c2a274c1bc
clearwinggames <80926717+clearwinggames@users.noreply.github.com>
2026-02-05 10:23:51 -0500

    Update sampleWebComponent.js

--- MFE/sampleWebComponent.js
+++ MFE/sampleWebComponent.js
@@ -22,17 +22,47 @@ class MyComponent extends HTMLElement {
                 }
             </style>
             <div class="container">
-                <p class="greeting">Hello, <span id="name"></span>!</p>
+                <p class="greeting">Hello, <span id="name">${name}</span>!</p>
             </div>
         `;
     }
 
+    static get observedAttributes() {
+     return ['name'];
+    }
+    
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+
+render() {
+    const name = this.getAttribute('name') || 'Guest';
+        this.shadowRoot.innerHTML = `
+            <style>
+                .container {
+                    padding: 10px;
+                    border: 1px solid #ccc;
+                    border-radius: 5px;
+                    background-color: #f9f9f9;
+                }
+                .greeting {
+                    color: blue;
+                    font-size: 18px;
+                }
+            </style>
+            <div class="container">
+                <p class="greeting">Hello, <span id="name">${name}</span>!</p>
+            </div>
+        
+  }
+
     // A lifecycle method called when the element is added to the document's DOM
     connectedCallback() {
+        this.render();/*
         const nameSpan = this.shadowRoot.querySelector('#name');
         // Get the 'name' attribute from the HTML and set the text content
         const name = this.getAttribute('name') || 'Guest';
-        nameSpan.textContent = name;
+        nameSpan.textContent = name;*/
     }
 }
 
